d82939ad70a01be7c64d47cc062cdce93681ef67
Aashiish-Pandey <ash006pandey@gmail.com>
2020-10-04 11:28:38 +0530

    Event Bubbling 1

--- main.js
+++ main.js
@@ -1,23 +1,9 @@
-let cleanRoom = function () {
-  return new Promise(function (resolve, reject) {
-    resolve("cleaned the room");
-  });
-};
-
-let removeGarbage = function (message) {
-  return new Promise(function (resolve, reject) {
-    resolve(message + " remove Garbage ");
-  });
-};
-
-let winIcecream = function (message) {
-  return new Promise(function (resolve, reject) {
-    resolve(message + " won Icecream");
-  });
-};
-
-//// if we want to perform anyting or execute anything only  after all the promises gets completed
-
-Promise.all([cleanRoom, removeGarbage, winIcecream]).then(function () {
-  console.log("all finsihed");
-});
+document
+  .querySelector("#grandparent")
+  .addEventListener("click", () => console.log("grandparent clicked"));
+document
+  .querySelector("#parent")
+  .addEventListener("click", () => console.log("parent clicked"));
+document
+  .querySelector("#child")
+  .addEventListener("click", () => console.log("child clicked"));
