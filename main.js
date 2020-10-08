a5c8cd54d061ed4a8cafd15a6be767bfe609ea9c
Aashiish-Pandey <ash006pandey@gmail.com>
2020-10-08 09:31:29 +0530

    without using async, await(using promises)

--- main.js
+++ main.js
@@ -1,15 +1,37 @@
-var x = function(j) {
+console.log('person1: shows ticket')
+console.log('person2: shows ticket')
 
-  this.i = 0
-  this.j = j
-}
 
-x.prototype.getj = function() {
+const promisewifeBringingTicks = new  Promise((resolve,reject) => {
 
-  return this.j
-}
+  setTimeout(()=>{
 
-var x1 = new x(1)
-var x2 = new x(2)
+    resolve("ticket")
+  },3000)
+})
 
-console.log(x1,x2)
+const getPopcorn = promisewifeBringingTicks.then((t)=>{
+  
+  console.log("wife: I have the ticks")
+  console.log("we should go in")
+  console.log("wife: no I am hungry")
+  return new Promise((resolve,reject) => resolve(`${t} popcorn`))
+
+  
+})
+
+const getButter = getPopcorn.then((t)=> {
+
+  console.log("Husband: I have got some popcorn")
+  console.log("Husband: we should go in")
+  console.log("Wife: I need butter on my popcorn")
+  return new Promise((resolve,reject) => {
+
+    resolve(`${t} butter`)
+  })
+})
+
+getButter.then((t) => console.log(t))
+
+console.log('person 4 shows ticket')
+console.log('person 5 shows ticket')
