4d0cd46bd66616b17f84ed3b3b3abdda336be4c0
Aashiish-Pandey <ash006pandey@gmail.com>
2021-03-23 18:17:00 +0530

    Prototype

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
-const myFunction = (inString, word, pos=-1) => {
-   
-    let inArray = inString.split(" ")
+// function doSomething(){}
+// doSomething.prototype.foo = "bar"; // add a property onto the prototype
+// var doSomeInstancing = new doSomething();
+// doSomeInstancing.prop = "some value"; // add a property onto the object
+// console.log( doSomeInstancing );
 
-  if (pos ===-1) {
-    inArray.splice(0,0,word)
-  } else {
+// function doSomething(){}
+// doSomething.prototype.foo = "bar";
+// var doSomeInstancing = new doSomething();
+// doSomeInstancing.prop = "some value";
+// console.log("doSomeInstancing.prop:      " + doSomeInstancing.prop);
+// console.log("doSomeInstancing.foo:       " + doSomeInstancing.foo);
+// console.log("doSomething.prop:           " + doSomething.prop);
+// console.log("doSomething.foo:            " + doSomething.foo);
+// console.log("doSomething.prototype.prop: " + doSomething.prototype.prop);
+// console.log("doSomething.prototype.foo:  " + doSomething.prototype.foo);
 
-    inArray.splice(pos,0,word)
-  }
-return inArray.join(" ")
-};
+let arr = ["ashish", "Pandey"]
 
-console.log(myFunction("I love my India", "why", 3));
+let object = {
+name : "ashish",
+city:"allahabad",
+getIntro: function() {
+
+  //console.log(this.name + "from" + this.city); 
+}
+}
+
+console.log(Array.prototype)
