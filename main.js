2fff5903ad9a3916c47cc9b3e4ada9b4d152de5f
Aashiish-Pandey <ash006pandey@gmail.com>
2021-03-24 07:54:34 +0530

    call apply bind

--- main.js
+++ main.js
@@ -1,29 +1,25 @@
-// function doSomething(){}
-// doSomething.prototype.foo = "bar"; // add a property onto the prototype
-// var doSomeInstancing = new doSomething();
-// doSomeInstancing.prop = "some value"; // add a property onto the object
-// console.log( doSomeInstancing );
+// call apply and bind methods:
 
-// function doSomething(){}
-// doSomething.prototype.foo = "bar";
-// var doSomeInstancing = new doSomething();
-// doSomeInstancing.prop = "some value";
-// console.log("doSomeInstancing.prop:      " + doSomeInstancing.prop);
-// console.log("doSomeInstancing.foo:       " + doSomeInstancing.foo);
-// console.log("doSomething.prop:           " + doSomething.prop);
-// console.log("doSomething.foo:            " + doSomething.foo);
-// console.log("doSomething.prototype.prop: " + doSomething.prototype.prop);
-// console.log("doSomething.prototype.foo:  " + doSomething.prototype.foo);
+let Name1  = {
 
-let arr = ["ashish", "Pandey"]
-
-let object = {
-name : "ashish",
-city:"allahabad",
-getIntro: function() {
-
-  //console.log(this.name + "from" + this.city); 
-}
+  fName:"ashish",
+  lName:"pandey"
 }
 
-console.log(Array.prototype)
+let Name2 = {
+
+  fName:"Shivam",
+  lName:"Pandey"
+}
+
+let printFullName = function(homeTown, state) {
+
+  console.log(this.fName + " " + this.lName +"from" +homeTown +state)
+
+
+}
+
+printFullName.call(Name1,"allahabad","up")
+printFullName.apply(Name2,["allahabad","uttarpradesh"])
+let returnedF=printFullName.bind(Name2,["katnai","Phoolpur"])
+returnedF()
