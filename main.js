8f9be60d89df7b54deb7dd9ddf7faab4511cf216
Aashiish-Pandey <ash006pandey@gmail.com>
2021-06-28 10:32:23 +0530

    sum(1)(2)(3)(4)..( n)()

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-// flattning the nested array
+// sum(1)(2)(3)(4)..( n)() | Amazon UI/Frontend Javascript Interview Question
 
-const isPalindrome = (inNum) => {
-  const num = inNum;
-  let newNum = 0;
-  let rem = 0;
-const arr = [1,2,[1,2,[2,3,[4,5]]]]
+const sum = function (a) {
+  return function (b) {
+    if (b) {
+      return sum(a + b);
+    } else {
+      return a;
+    }
+  };
+};
+
+console.log(sum(1)(2)(3)());
+
+
+// const sum = (a) => {
+//   return function (b) {
+//     return b ? sum(a + b) : a;
+//   };
+// };
+
+// console.log(sum(1)(2)(3)());
+
+
+// const sum = (a) => (b)=> b ?sum(a+b) :a
+  
+//   console.log(sum(1)(2)(3)(4)());
