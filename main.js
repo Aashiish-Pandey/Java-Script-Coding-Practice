03cf1d0ee1c92a216e8867636a85238fc5227937
Aashiish-Pandey <ash006pandey@gmail.com>
2021-07-13 11:20:51 +0530

    better

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-//missing number 
+//missing number
 
-const arr = [1,2,4,5,6,7,8,9]
-const len = arr.length-1
-const maxEl =arr[len]
+const arr = [1, 2, 4, 5, 6, 7, 8, 9];
+const len = arr.length - 1;
+const maxEl = arr[len];
 
-const total = maxEl*(maxEl+1)
-const sum = total/2
+const sum = (maxEl * (maxEl + 1)) / 2;
 
-const arrSum = arr.reduce((ca,ac)=>ca+ac)
+const arrSum = arr.reduce((ca, ac) => ca + ac);
 
-const missingEl = sum-arrSum
-console.log(sum)
-console.log(arrSum)
-console.log(missingEl)
+const missingEl = sum - arrSum;
+console.log(sum);
+console.log(arrSum);
+console.log(missingEl);
