1d06c062b2c1317ec003e2e37a2ab4ad9add05e4
Aashiish-Pandey <ash006pandey@gmail.com>
2021-07-13 11:13:28 +0530

    missing number from an array

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-let x = 12345656;
-let count = 0;
+//missing number 
 
-while (x) {
-  x = Math.floor(x / 10);
-  count++;
-}
+const arr = [1,2,4,5,6,7,8,9]
+const len = arr.length-1
+const maxEl =arr[len]
 
-console.log(count);
+const total = maxEl*(maxEl+1)
+const sum = total/2
+
+const arrSum = arr.reduce((ca,ac)=>ca+ac)
+
+const missingEl = sum-arrSum
+console.log(sum)
+console.log(arrSum)
+console.log(missingEl)
