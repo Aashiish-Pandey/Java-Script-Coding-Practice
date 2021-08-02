500ef39f9b8080f36a919c23c01b40b6aa31458a
Aashiish-Pandey <ash006pandey@gmail.com>
2021-08-02 10:22:21 +0530

    area ,perimeter and Diameter without using Higher ord Fun

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
-//unwarp a nested array without using predefined methods
+const readius = [1, 2, 3, 4];
 
-const arr = [1, 2, [1, 2, [2, 3, [4, 5,6,7,8,9]]]];
+const calculateArea = function (inArr) {
+  let output = [];
 
-const flatArray = (inArr) => {
-  const arrStack = inArr;
-  const flatArray = [];
-
-  while (arrStack.length) {
-    let stkEl = arrStack.pop();
-    if (Array.isArray(stkEl)) {
-      arrStack.push(...stkEl);
-    } else {
-      flatArray.push(stkEl);
-    }
+  for (let i = 0; i < inArr.length; i++) {
+    output.push(Math.PI * inArr[i] * inArr[i]);
   }
 
-  return flatArray.reverse();
+  return output;
 };
 
-console.log(flatArray(arr));
+const calculatePerimter = function (inArr) {
+  const output = [];
+  for (let i = 0; i < inArr.length; i++) {
+    output.push(2 * Math.PI * inArr[i]);
+  }
+  return output;
+};
+
+const calculateDiameter = function (inArr) {
+  const output = [];
+  for (let i = 0; i < inArr.length; i++) {
+    output.push(2 * inArr[i]);
+  }
+  return output;
+};
+
+console.log(calculateArea(readius));
+console.log(calculatePerimter(readius));
+console.log(calculateDiameter(readius));
